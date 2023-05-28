56c96379719e5ba2010a43f61b8c4f15f95e8a5f
yle <levany.work@gmail.com>
2023-05-28 20:43:42 +0700

    update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apiCreateSession": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "reactlivenessb52bc4dbb52bc4db": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "createSession": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ThemeProvider, Loader } from "@aws-amplify/ui-react";
+
+import "@aws-amplify/ui-react/styles.css";
+
+import { FaceLivenessDetector } from "@aws-amplify/ui-react-liveness";
+import awsexports from "./aws-exports";
+import Rekognition from "aws-sdk/clients/rekognition";
+import { Amplify } from "aws-amplify";
+import { Auth } from "@aws-amplify/auth";
+import { useState, useEffect } from "react";
+
+Amplify.configure(awsexports);
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [createLivenessApiData, setCreateLivenessApiData] = useState(null);
+
+  async function getRekognitionClient(credentials) {
+    const rekognitionClient = new Rekognition({
+      region: "ap-southeast-1",
+      credentials,
+      endpoint: "https://rekognition.ap-southeast-1.amazonaws.com",
+    });
+
+    return rekognitionClient;
+  }
+
+  useEffect(() => {
+    console.log("vao day");
+    if (createLivenessApiData) return;
+
+    Auth.currentCredentials()
+      .then(async (credentials) => {
+        console.log("credentials", credentials);
+        const rekognition = await getRekognitionClient(credentials);
+        const response = await rekognition
+          .createFaceLivenessSession()
+          .promise();
+        setCreateLivenessApiData(response.SessionId);
+        setLoading(false);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  const handleAnalysisComplete = async () => {
+    console.log("result");
+
+    Auth.currentCredentials()
+      .then(async (credentials) => {
+        console.log("credentials", credentials);
+        const rekognition = await getRekognitionClient(credentials);
+        const response = await rekognition
+          .getFaceLivenessSessionResults()
+          .promise();
+        console.log("response result", response);
+      })
+      .catch((e) => console.error(`result error`, e));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <span>{`sessionId: ${createLivenessApiData}`}</span>
+          <FaceLivenessDetector
+            sessionId={createLivenessApiData}
+            region="ap-southeast-1"
+            onAnalysisComplete={handleAnalysisComplete}
+          />
+        </>
+      )}
+    </ThemeProvider>
   );
 }
 
